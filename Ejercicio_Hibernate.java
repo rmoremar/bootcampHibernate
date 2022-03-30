3640d58fce213bdcbace793c36c1d1fa2b298132
rmoremar <rmoremar@emeal.nttdata.com>
2022-03-30 09:02:50 +0200

    first commit

---
+++ Ejercicio_Hibernate.java
@@ -0,0 +1,42 @@
+package pruebas;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/*
+ * raul moreno martinez
+ */
+public class Ejercicio_Hibernate {
+	
+	private Connection conexion;
+	
+	public Ejercicio_Hibernate() {
+		
+		if (establecerConexion() == 0) {
+			try {
+				PreparedStatement prepareStatement = conexion.prepareStatement("");
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			System.err.println("No se puede conectar");
+		}
+		
+	}
+	
+	private int establecerConexion() {
+		String url = "jdbc:mysql:nttdata_jdbc_ex";
+		String user = "usuario";
+		String pass = "12345678";
+		try {
+			conexion = DriverManager.getConnection(url,user,pass);
+			return 0;
+		} catch (SQLException e) {
+			return 1;
+		}
+	}
+	
+}
